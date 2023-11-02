c8b50fe701331942f9336c1ab1f3dc591b517918
julydeath <manojkarajada.mk@gmail.com>
2023-11-02 12:58:49 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const para = document.querySelector("p");
+console.log(para.innerText);
+para.innerText = "Hello";
+const allPara = document.querySelectorAll("p");
+const classes = document.querySelectorAll(".test");
+console.log(classes);
